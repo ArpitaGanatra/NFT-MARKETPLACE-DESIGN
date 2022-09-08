43d890d18a8dc616eb97d26a3a4eafcbbd771480
ArpitaGanatra <ganatraarpita@gmail.com>
2022-09-08 19:21:16 +0530

    hero section

---
+++ src/components/ui/HeroSection.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Col, Container, Row } from "reactstrap";
+import heroImg from "../../images/hero.jpg";
+import "./hero-section.css";
+
+const HeroSection = () => {
+  return (
+    <section className="hero__section">
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className="hero__content">
+              <h2>
+                Discover rare digital art and collect{" "}
+                <span>sell extraordinary</span> NFTs
+              </h2>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Excepturi velit non ipsa dolorum eaque quasi dicta, atque nam
+                veniam omnis.
+              </p>
+              <div className="hero__btns d-flex align-items-center gap-4">
+                <button className="d-flex align-items-center gap-2">
+                  <i class="ri-rocket-line"></i>
+                  <Link to="/market">Explore</Link>
+                </button>
+                <button className="d-flex align-items-center gap-2">
+                  <i class="ri-ball-pen-line"></i>
+                  <Link to="/create">Create</Link>
+                </button>
+              </div>
+            </div>
+          </Col>
+          <Col lg="6" md="6">
+            <div className="hero__img">
+              <img src={heroImg} alt="hero image" className="w-100"></img>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default HeroSection;
